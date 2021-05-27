3e503e2f2926078acd97406d9ca3bee9f3a29dd6
Dmytro <dimagolyak@gmail.com>
2021-05-27 21:40:42 +0300

    Added middleware

---
+++ middleware/file.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'public/books')
+    },
+    filename(req, file, cb) {
+        cb(null, `${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}`)
+    }
+});
+
+const allowedTypes = ['txt', 'application/pdf'];
+
+const fileFilter = (req, file, cb) => {
+    console.log('file', file);
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+};
+
+module.exports = multer({
+    storage, fileFilter
+});
